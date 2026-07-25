ccea46544155c2ba3b95d0744790d714c30e5857
khalid11-11 <edu.khalid@outlook.com>
2026-07-25 19:53:05 +0300

    Create Doctor table

--- SmartClinicDB.sql
+++ SmartClinicDB.sql
@@ -22,3 +22,14 @@ CREATE TABLE Patient (
     CONSTRAINT fk_patient_person
         FOREIGN KEY (PersonID) REFERENCES Person(PersonID)
 );
+
+CREATE TABLE Doctor (
+    DoctorID INT AUTO_INCREMENT PRIMARY KEY,
+    PersonID INT NOT NULL,
+    Specialization VARCHAR(100) NOT NULL,
+    DepartmentID INT NOT NULL,
+    CONSTRAINT fk_doctor_person
+        FOREIGN KEY (PersonID) REFERENCES Person(PersonID),
+    CONSTRAINT fk_doctor_department
+        FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)
+);
